de57da7f37c861632d35b414f11f67862494e074
Peter Ngacha <peterngacha85@gmail.com>
2025-07-03 11:52:27 +0300

    Initial commit

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
+
+const Greeting = () => {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get(`${import.meta.env.VITE_API_URL}/`)
+      .then(res => setMessage(res.data))
+      .catch(err => setMessage(`Error loading message: ${err.message}`));
+  }, []);
+
+  return (
+    <div>
+      <h2>Greeting from Backend:</h2>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default Greeting;
