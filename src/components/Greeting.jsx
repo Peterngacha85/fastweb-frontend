92986019ec40a6fe13be2aa11ad199cb7f5a39fe
Peter Ngacha <peterngacha85@gmail.com>
2025-07-03 13:11:04 +0300

    style: apply homepage design with new theme color and layout

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,15 +1,14 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-
-
 const Greeting = () => {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    axios.get(`${import.meta.env.VITE_API_URL}/`)
-      .then(res => setMessage(res.data))
-      .catch(err => setMessage(`Error loading message: ${err.message}`));
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/`)
+      .then((res) => setMessage(res.data))
+      .catch((err) => setMessage(`Error loading message: ${err.message}`));
   }, []);
 
   return (
